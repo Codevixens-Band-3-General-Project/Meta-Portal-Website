860d8bd1ba023861015958441af3745abd1d9f7c
Sharon <oitosharonakoth@gmail.com>
2023-07-07 16:41:29 +0300

    navbar,footer,button

--- meta-portal-website/src/App.js
+++ meta-portal-website/src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
+import Footer from "./componets/Footer";
+import Navbar from "./componets/Navbar";
+import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Collection from './components/Collection';
+// import Blog from './components/Blog';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <div className="font-bold">Meta Portal Website</div>
-    </div>
+<BrowserRouter>
+<Navbar />
+
+
+<Footer/>
+  <Routes>
+    <Route></Route>
+    {/* <Route path="/" element={<Home />} />
+    <Route path="/" element={<About />}/>
+    <Route path="/" element={<Collection/>} /> 
+    <Route path="/" element={<Blog/>} /> 
+    <Route path="/" element={<Contact />} />  */}
+  </Routes>
+  </BrowserRouter>
   );
 }
 
 export default App;
+
+
---
+++ meta-portal-website/src/componets/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Button = ({ children, onClick, className }) => {
+  return (
+    <button
+        className={`bg-transparent text-white font-bold py-2 px-4 rounded border-2 border-purple-600 hover:border-purple-700 ${className}`}
+
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
+
+// #17102c
