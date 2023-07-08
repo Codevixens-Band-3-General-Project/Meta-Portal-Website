0627c3216eb2771a90413bcbd7f37f33f341962f
Bashua Mutiat <mutiatbashua@gmail.com>
2023-07-08 09:58:40 +0100

    added page divider

--- meta-portal-website/src/App.js
+++ meta-portal-website/src/App.js
@@ -1,37 +1,38 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import React from 'react';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/homepage";
-import About from './components/About';
-import Collections from './components/collections';
+import About from "./components/About";
+import Collections from "./components/collections";
+import PageDivider from "./components/page_divider";
 // import Contact from './components/Contact';
 // import Blog from './components/Blog';
 
-
 const App = () => {
   return (
-<div className="App">
-<BrowserRouter>
-<Navbar />
-<Home/>
-<About/>
-<Collections/>
-<Footer/>
-  <Routes>
-    <Route></Route>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Home />
+        <About />
+        <PageDivider />
+        <Collections />
+        <PageDivider />
+        <Footer />
+        <Routes>
+          <Route></Route>
 
-
-    {/* <Route path="/about" element={<About />}/> */}
-    {/* <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />}/> */}
+          {/* <Route path="/" element={<Home />} />
     <Route path="/" element={<About />}/>
     <Route path="/" element={<Collection/>} /> 
     <Route path="/" element={<Blog/>} /> 
     <Route path="/" element={<Contact />} />  */}
-  </Routes>
-  </BrowserRouter>
-  </div>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
